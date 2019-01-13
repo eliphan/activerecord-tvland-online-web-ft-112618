57582fdf310ef52508fb597d1ad2ee1310ed7553
Elise Phan <aliceconnic@gmail.com>
2019-01-13 09:11:54 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_data_to_show.rb
+++ db/migrate/009_add_data_to_show.rb
@@ -1,4 +1,4 @@
-class AddDayAndSeasonToShow < ActiveRecord::Migration[5.1]
+class AddDataToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
